8aaec2dd6703f69d568321526dd3a6c0bc28ee7b
jlavrin <julian.lavrin@icloud.com>
2024-05-28 20:27:18 +0200

    feat: go air - fast refreshes at dev

---
+++ server/cmd/app/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"github.com/gorilla/websocket"
+	"log"
+	"net/http"
+)
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+func main() {
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		a := r.Header.Get("Authorization")
+
+		// auth to do
+		if a == "" {
+			http.Error(w, "Unauthorized", http.StatusUnauthorized)
+			return
+		}
+
+		conn, err := upgrader.Upgrade(w, r, nil)
+
+		if err != nil {
+			log.Println(err, "test")
+			return
+		}
+
+		defer conn.Close()
+
+		for {
+			_, message, err := conn.ReadMessage()
+			if err != nil {
+				log.Println(err)
+				return
+			}
+			log.Printf("received: %s", message)
+
+			if err := conn.WriteMessage(websocket.TextMessage, message); err != nil {
+				log.Println(err)
+			}
+		}
+	})
+
+	log.Println("[] %s", "1")
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
